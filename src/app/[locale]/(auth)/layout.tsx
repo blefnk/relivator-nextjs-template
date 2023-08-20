f3ce181f3d9844ba718a3aef7fadcd03776fdffe
blefnk <blefnk@gmail.com>
2023-08-21 00:59:52 +0200

    🧹 feat(app): temporary remove image

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -14,14 +14,14 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
       <AspectRatio ratio={16 / 9}>
-        <Image
+        {/* <Image
           src="/images/auth-layout.webp"
           alt="A skateboarder doing a high drop"
           fill
           className="absolute inset-0 object-cover"
           priority
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+        /> */}
         <div className="absolute inset-0 bg-gradient-to-t from-background to-background/60 md:to-background/40" />
         <Link
           href="/"
